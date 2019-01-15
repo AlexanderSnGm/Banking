90e480a01269ee266a1cfefb35aaae04ef772cf9
AlexanderSnGm <sander_31s@hotmail.com>
2019-01-15 15:48:03 -0500

    desc

---
+++ Banking/Banking/Banking/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Banking.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Banking
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            //MainPage = new MainPage();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Banking/Banking/Banking/Views/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Banking.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += async (s, e) => {
+                // handle the tap
+                await Navigation.PushAsync(new ForgotLoginInfoPage());
+            };
+            lbForgotPass.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        async void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+
+        async void BtnSignup_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
